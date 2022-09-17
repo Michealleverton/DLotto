48de7c2e46a37b0561346e793c08b94e052cf408
Micheal Leverton <levertonmicheal@gmail.com>
2022-09-17 19:58:45 -0400

    updated timers

--- src/FFcountdownTimer.js
+++ src/FFcountdownTimer.js
@@ -1,6 +1,6 @@
 
 // Setup End Date for Countdown (getTime == Time in Milleseconds)
-let launchDate = new Date("SEPT 18, 2022 21:00:00").getTime();
+let launchDate = new Date("SEPT 23, 2022 21:00:00").getTime();
 
 // Setup Timer to tick every 1 second
 setInterval(tick, 1000);
